5a5b22ad6525055ce58c2616bdcb1f71c6916605
Talal <talalahmad257@gmail.com>
2022-12-09 11:31:50 +0000

    Allow repliedUser to be pinged

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const client = new Client({
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMembers,
   ],
-  allowedMentions: { parse: [] },
+  allowedMentions: { parse: [], repliedUser: true },
 });
 
 const api = new ChatGPTAPI({
